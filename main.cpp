8da953ba0946ce0704aa293cf629e6a471a48838
Steph Huerta <stph.flwrs@gmail.com>
2015-03-09 21:05:24 -0500

    -Small edits to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,8 @@ int main() {
     graph_maker generator("input_files/point_to_point.lotr", "input_files/straight_line_distances.lotr");
     
     generator.create_graph();
+    graph<string, road> paths_graph = generator.get_graph();
+    
+    paths_graph.print();
     return 0;
 }
